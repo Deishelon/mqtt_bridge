3728276fcd17dc83dc8da365390a0c91cf9017b8
Nikita Pustovoi <deishelon@gmail.com>
2022-06-25 19:51:11 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# MQTT Bridge
+
+Simple implementation of one way MQTT bridge.  
+The goal of this project is to provide a simple way to bridge topics from one broker onto another broker.  
+
+I have developed this, as I ran main MQTT server at home, but there are some applications that need to run in own broker, 
+however, I wanted to make some parts available on my home main broker.
+This script should be fully autonomous, i.e. it will handle reconnections if network goes down.
+
+### Usage
+1. Create virtual environment:
+```bash
+python3 -m venv venv
+```
+2. Activate it
+```bash
+source venv/bin/activate
+```
+3. Install dependencies
+```bash
+pip install -r requirements.txt
+```
+4. Run it
+```bash
+python bridge.py config_file_path.yml
+```
+
+### Config file
+Config file must be passed as the only argument to the script,  
+the content of the file is:
+
+Example file:
+```yaml
+bridge-from:
+   host: source.home
+   port: 1883
+   user: 'my_user' # Can be removed if no auth is used
+   password: 'my_password' # Can be removed if no auth is used
+   topic: 'source_topic/#'
+
+
+bridge-to:
+   host: destination.home
+   port: 1883
+   user: 'my_user' # Can be removed if no auth is used
+   password: 'my_password' # Can be removed if no auth is used
+   topic: 'destination_topic'
+```
+
+| Key      | Meaning (Common)                                    | Meaning for 'bridge-from'                                    | Meaning for 'bridge-to'                                                                                                                                                                      |
+|----------|-----------------------------------------------------|--------------------------------------------------------------|----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+| host     | The IP or DNS name of the broker                    |                                                              |                                                                                                                                                                                              |
+| post     | The port of the broker                              |                                                              |                                                                                                                                                                                              |
+| user     | Optional key to provide user name if auth is needed |                                                              |                                                                                                                                                                                              |
+| password | Optional key to provide password if auth is needed  |                                                              |                                                                                                                                                                                              |
+| topic    | x                                                   | Topic to bridge from the source broker<br/> Wildcards are ok | For every incoming message from source broker this topic will be appended<br/> If bridge to root, leave empty string,<br/>if bridge to non-root location make sure to leave out trailing '/' |
+
---
+++ bridge.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+import random
+import sys
+import time
+import yaml
+from paho.mqtt import client as mqtt_client
+
+
+def connect_mqtt(client_id, username, password, host):
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print(f"Connected to MQTT Broker {host}")
+        else:
+            print(f"Failed to connect {host}, return code %d\n", rc)
+
+    client = mqtt_client.Client(client_id)
+    client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    return client
+
+
+def publish(client: mqtt_client, payload, topic: str):
+    result = client.publish(topic, payload)  # result: [0, 1]
+    status = result[0]
+    if status == 0:
+        print(f"Send `{payload}` to topic `{topic}`")
+    else:
+        print(f"Failed to send message to topic {topic}")
+
+
+def subscribe(client: mqtt_client, topic: str, on_message):
+    client.subscribe(topic)
+    client.on_message = on_message
+
+
+def start_bridge(bridge_config):
+    from_host = bridge_config['bridge-from']['host']
+    from_port = bridge_config['bridge-from']['port']
+    from_user = bridge_config['bridge-from'].get("user")
+    from_password = bridge_config['bridge-from'].get("password")
+    from_client_id = f'python-bridge-mqtt-{random.randint(0, 1000)}'
+    from_topic = bridge_config['bridge-from']['topic']
+
+    to_host = bridge_config['bridge-to']['host']
+    to_port = bridge_config['bridge-to']['port']
+    to_user = bridge_config['bridge-to'].get("user")
+    to_password = bridge_config['bridge-to'].get("password")
+    to_client_id = f'python-bridge-mqtt-{random.randint(0, 1000)}'
+    to_topic = bridge_config['bridge-to']['topic']
+
+    to_connection = None
+    from_connection = None
+
+    def on_message_from_source_broker(client, userdata, msg):
+        decoded_msg = msg.payload.decode()
+        print(f"Received `{decoded_msg}` from `{msg.topic}` topic")
+        if to_connection.is_connected():
+            destination_topic = f"{to_topic}/{msg.topic}"
+            print(f"Forwarding message to destination broker at {destination_topic}")
+            publish(to_connection, decoded_msg, destination_topic)
+            pass
+
+    while True:
+        try:
+            if to_connection is None:
+                to_connection = connect_mqtt(to_client_id, to_user, to_password, to_host)
+
+            if from_connection is None:
+                from_connection = connect_mqtt(from_client_id, from_user, from_password, from_host)
+
+            if not to_connection.is_connected():
+                try:
+                    to_connection.connect(to_host, to_port)
+                except Exception as e:
+                    print(f"Error making connection to {to_host}, {e}")
+                    time.sleep(1)
+
+            if not from_connection.is_connected():
+                try:
+                    from_connection.connect(from_host, from_port)
+                except Exception as e:
+                    print(f"Error making connection to {from_host}, {e}")
+                    time.sleep(1)
+                subscribe(from_connection, from_topic, on_message_from_source_broker)
+
+            if to_connection is not None:
+                to_connection.loop()
+
+            if from_connection is not None:
+                from_connection.loop()
+        except InterruptedError:
+            return
+    pass
+
+
+def main(config_file_location: str):
+    with open(config_file_location, "r") as stream:
+        try:
+            bridge_config = yaml.safe_load(stream)
+            print(bridge_config)
+            start_bridge(bridge_config)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print(f"Pass config file path as the only parameter to the application")
+        print(f"Usage: '{sys.argv[0]} bridge_config.yml'")
+        pass
+    else:
+        print(f"Starting bridge with `{sys.argv[1]}` config file")
+        main(sys.argv[1])
